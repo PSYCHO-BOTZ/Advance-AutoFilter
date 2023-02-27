7579a4bbef9ecc1890e06ce959150a58ccae5866
Psycho Botz <124176508+PSYCHO-BOTZ@users.noreply.github.com>
2023-02-27 11:07:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/EvaMaria /EvaMaria
+  git clone https://github.com/PSYCHO-BOTZ/Advance-AutoFilter.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
